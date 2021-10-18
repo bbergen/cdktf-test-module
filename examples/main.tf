ad63a0dee71fcb9b1b82366a2ce911e80328869d
Bryan Bergen <bryan.bergen@qhrtech.com>
2021-10-18 15:16:35 -0700

    added example

---
+++ examples/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "~> 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.56"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
+
+module "test_storage_account" {
+  source               = "../"
+  resource_group_name  = "test-bbergen"
+  storage_account_name = null
+  table_name           = "test_table01"
+}
+
